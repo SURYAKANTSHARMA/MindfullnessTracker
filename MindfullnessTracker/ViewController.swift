4ef4afc989157757e27e671a6e41614e95e31dbe
SuryaKant Sharma <suryakantsharma84@gmail.com>
2018-11-04 11:57:49 +0530

    Initial Commit

---
+++ MindfullnessTracker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MindfullnessTracker
+//
+//  Created by SuryaKant Sharma on 04/11/18.
+//  Copyright © 2018 SuryaKant Sharma. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
